b5bfa08e9fadac8f01739282c0bd02f24aa9474b
Eric T <Eric@SPIN-WIN>
2018-06-18 21:09:35 -0500

    Add project files.

---
+++ Gopherit/Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(697, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(69, 27)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Go"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(11, 45)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(118, 30)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "What's New?"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(135, 45)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(118, 30)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "What's Cool?"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(259, 45)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(142, 30)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Gopher Project"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(407, 45)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(127, 30)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "Floodgap"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(11, 81)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(755, 505)
+        Me.TabControl1.TabIndex = 6
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.WebBrowser1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 29)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(747, 472)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Gophermap"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebBrowser1.Location = New System.Drawing.Point(3, 3)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(741, 466)
+        Me.WebBrowser1.TabIndex = 2
+        Me.WebBrowser1.Url = New System.Uri("about:blank", System.UriKind.Absolute)
+        Me.WebBrowser1.WebBrowserShortcutsEnabled = False
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.TextBox2)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 29)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(747, 472)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Source"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.White
+        Me.TextBox2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox2.Font = New System.Drawing.Font("Courier New", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(3, 3)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox2.Size = New System.Drawing.Size(741, 466)
+        Me.TextBox2.TabIndex = 0
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(63, 12)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(628, 28)
+        Me.ComboBox1.TabIndex = 7
+        Me.ComboBox1.Text = "gopher://gopher.floodgap.com"
+        '
+        'Button6
+        '
+        Me.Button6.Enabled = False
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(12, 12)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(45, 27)
+        Me.Button6.TabIndex = 8
+        Me.Button6.Text = "<"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(778, 598)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Gopherit"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button6 As Button
+End Class
---
+++ Gopherit/Form1.vb
@@ -0,0 +1,133 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Public curl = "curl.exe"
+
+    Public Shared Function FindCurl()
+        Dim fileName = "curl.exe"
+        If File.Exists(fileName) Then Return Path.GetFullPath(fileName)
+        Dim values = Environment.GetEnvironmentVariable("PATH")
+
+        For Each path In values.Split(";"c)
+            Dim fullPath = path & fileName
+            If File.Exists(fullPath) Then Return fullPath
+        Next
+
+        Return Nothing
+    End Function
+
+    Public Sub Go(sender As Object, e As EventArgs) Handles Button1.Click
+        If Not ComboBox1.Text.Contains("://") Then
+            ComboBox1.Text = "gopher://" & ComboBox1.Text
+        End If
+
+        Dim oProcess As New Process()
+        Dim oStartInfo As New ProcessStartInfo(curl, ComboBox1.Text)
+        oStartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        oStartInfo.UseShellExecute = False
+        oStartInfo.RedirectStandardOutput = True
+        oProcess.StartInfo = oStartInfo
+        oProcess.Start()
+
+        Dim sOutput As String
+        Using oStreamReader As System.IO.StreamReader = oProcess.StandardOutput
+            sOutput = oStreamReader.ReadToEnd()
+        End Using
+        TextBox2.Text = sOutput
+
+        Dim strReader As New IO.StringReader(TextBox2.Text)
+        Dim CurrentLine = strReader.ReadLine
+
+        Dim html = "<html><head><style>.i{color:green}.h{color:blue}.0 a{color:gray}.1 a{color:orange}.7 a{color:purple}</style></head><body>"
+
+        Do While (Not CurrentLine Is Nothing)
+
+            Try
+                CurrentLine = CurrentLine.Replace("error.host	1", "")
+
+                Dim CLArray = CurrentLine.Split(vbTab)
+
+                If CLArray(0).StartsWith("i") Then
+                    html = html & "<pre class='i' title='Information'>" & CLArray(0).Substring(1) & "</pre>"
+                ElseIf CLArray(0).StartsWith("h") Then
+                    html = html & "<pre class='h' title='HTML Document'><a target='_blank' href='" & CLArray(1).Replace("URL:", "") & "'>" & CLArray(0).Substring(1) & "</a></pre>"
+                ElseIf CLArray(0).StartsWith("0") Then
+                    '0Caltrans California highway conditions	/calroads	gopher.floodgap.com	70
+                    html = html & "<pre class='0' title='Text File'><a href='about:blank?url=gopher://" & CLArray(2) & ":" & CLArray(3) & "/" & CLArray(1) & "&txt=yes'>" & CLArray(0).Substring(1) & "</a></pre>"
+                ElseIf CLArray(0).StartsWith("1") Then
+                    html = html & "<pre class='1' title='Directory'><a href='about:blank?url=gopher://" & CLArray(2) & ":" & CLArray(3) & "/" & CLArray(1) & "'>" & CLArray(0).Substring(1) & "</a></pre>"
+                ElseIf CLArray(0).StartsWith("7") Then
+                    html = html & "<pre class='7' title='Search'><a href='about:blank?url=gopher://" & CLArray(2) & ":" & CLArray(3) & "/" & CLArray(1) & "&search=yes'>" & CLArray(0).Substring(1) & "</a></pre>"
+                Else
+                    html = html & "<pre title='Unknown'>" & CurrentLine & "</pre>"
+                End If
+            Catch ex As Exception
+            End Try
+
+            CurrentLine = strReader.ReadLine 'Reading the line for the next iteration
+        Loop
+
+        html = html & "</body></html>"
+
+
+        WebBrowser1.DocumentText = html
+
+        Dim cbtemp = ComboBox1.Text
+        If ComboBox1.Items.Contains(cbtemp) Then
+            ComboBox1.Items.Remove(cbtemp)
+        End If
+        ComboBox1.Items.Add(cbtemp)
+        ComboBox1.SelectedIndex = ComboBox1.Items.Count - 1
+        If ComboBox1.Items.Count > 1 Then
+            Button6.Enabled = True
+        Else
+            Button6.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        curl = FindCurl()
+
+        If curl = Nothing Then
+            MsgBox("Gopherit was unable to find curl.exe, the application used for getting resources from the Internet, in the working directory " & My.Computer.FileSystem.CurrentDirectory & " or your PATH. You will need to download a Generic binary from https://curl.haxx.se/download.html#Win32 and put it into the working directory.", MsgBoxStyle.Exclamation, "Curl not found")
+            Application.Exit()
+        End If
+
+        WebBrowser1.Navigate("about:blank")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ComboBox1.Text = "gopher://gopherproject.org"
+        Go(sender, e)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ComboBox1.Text = "gopher://gopher.floodgap.com"
+        Go(sender, e)
+    End Sub
+
+    Private Sub WebBrowser1_LocationChanged(sender As Object, e As EventArgs) Handles WebBrowser1.DocumentCompleted
+        Console.WriteLine(WebBrowser1.Url.ToString)
+        If WebBrowser1.Url.ToString.Replace("about:blank?url=", "").StartsWith("gopher://") Then
+            If WebBrowser1.Url.ToString.Replace("about:blank?url=", "").Contains("&txt=yes") Then
+                ComboBox1.Text = WebBrowser1.Url.ToString.Replace("about:blank?url=", "").Replace("&txt=yes", "")
+                TabControl1.SelectTab(1)
+                Go(sender, e)
+            ElseIf WebBrowser1.Url.ToString.Replace("about:blank?url=", "").Contains("&search=yes") Then
+                ComboBox1.Text = WebBrowser1.Url.ToString.Replace("about:blank?url=", "").Replace("&search=yes", "") & "?" & InputBox("Enter a query for the remote server to process.", "Query requested")
+                TabControl1.SelectTab(0)
+                Go(sender, e)
+            Else
+                ComboBox1.Text = WebBrowser1.Url.ToString.Replace("about:blank?url=", "")
+                TabControl1.SelectTab(0)
+                Go(sender, e)
+            End If
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        ComboBox1.SelectedIndex = ComboBox1.Items.Count - 2
+    End Sub
+End Class

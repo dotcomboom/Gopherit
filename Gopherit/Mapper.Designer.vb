26ad6ffa88d37679369a48707a330b4fd570798d
Eric T <Eric@SPIN-WIN>
2018-06-20 15:59:13 -0500

    Mapper

--- Gopherit/Mapper.Designer.vb
+++ Gopherit/Mapper.Designer.vb
@@ -22,29 +22,145 @@ Partial Class Mapper
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Mapper))
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(43, 38)
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(324, 277)
+        Me.TextBox1.Size = New System.Drawing.Size(401, 434)
         Me.TextBox1.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TextBox5)
+        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 321)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Insert"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.GroupBox1)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel2.Location = New System.Drawing.Point(398, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(200, 435)
+        Me.Panel2.TabIndex = 2
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"0 Text File", "1 Directory", "3 Error", "4 HQX filer", "5 PC binary", "6 UNIX uuencoded file", "7 Search Service", "8 Telnet session", "9 Binary file", "g GIF image", "s Sound", "I Generic image", "h HTML Document or Off-Gopher Resource"})
+        Me.ComboBox1.Location = New System.Drawing.Point(9, 25)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(179, 28)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(9, 59)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(179, 26)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Location = New System.Drawing.Point(9, 91)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(179, 26)
+        Me.TextBox3.TabIndex = 3
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Enabled = False
+        Me.TextBox4.Location = New System.Drawing.Point(9, 123)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(179, 26)
+        Me.TextBox4.TabIndex = 4
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Enabled = False
+        Me.TextBox5.Location = New System.Drawing.Point(9, 157)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(179, 26)
+        Me.TextBox5.TabIndex = 5
+        Me.TextBox5.Text = "70"
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(10, 190)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(178, 128)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Select an item type."
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(142, 282)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(46, 33)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "+"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Mapper
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(565, 452)
+        Me.ClientSize = New System.Drawing.Size(598, 435)
+        Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.TextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Mapper"
         Me.Text = "Mapper"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
 End Class
--- Gopherit/Mapper.vb
+++ Gopherit/Mapper.vb
@@ -1,3 +1,70 @@
 ﻿Public Class Mapper
 
+    Private Sub ComboBox1_SelectedValueChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedValueChanged
+        Dim itemtype = ComboBox1.SelectedItem.ToString.Substring(0, 1)
+        '0 Text File
+        '1 Directory
+        '3 Error
+        '4 HQX filer
+        '5 PC binary
+        '6 UNIX uuencoded file
+        '7 Search Service
+        '8 Telnet session
+        '9 Binary file
+        'g GIF image
+        's Sound
+        'I Generic image
+        'h HTML Document Or Off-Gopher Resource
+        Dim textpathserverport = {"0", "1", "4", "5", "6", "7", "9", "g", "s", "I", "h"}
+        Dim textidkserverport = {"8"}
+        Dim text = "3"
+        Label1.Text = "1. Text"
+        Button1.Enabled = True
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        TextBox5.Clear()
+        TextBox2.Enabled = True
+        TextBox3.Enabled = True
+        TextBox4.Enabled = True
+        TextBox5.Enabled = True
+        If textpathserverport.Contains(itemtype) Then
+            If itemtype = "h" Then
+                TextBox3.Text = "(URL: http://example.com)"
+                Label1.Text = Label1.Text & vbNewLine & "2. Path on server OR '(URL: http://example.com)'" & vbNewLine & "3. Server" & vbNewLine & "4. Port"
+            Else
+                Label1.Text = Label1.Text & vbNewLine & "2. Path on server" & vbNewLine & "3. Server" & vbNewLine & "4. Port"
+            End If
+        ElseIf textidkserverport.Contains(itemtype) Then
+            TextBox3.Text = "a"
+            TextBox3.Enabled = False
+            Label1.Text = Label1.Text & vbNewLine & "2. 'a'" & vbNewLine & "3. Server" & vbNewLine & "4. Port"
+        ElseIf text.Contains(itemtype) Then
+            TextBox3.Enabled = False
+            TextBox4.Enabled = False
+            TextBox5.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.AppendText(vbNewLine)
+        TextBox1.AppendText(ComboBox1.SelectedItem.ToString.Substring(0, 1))
+        TextBox1.AppendText(TextBox2.Text)
+        'can be disabled and "a"
+        TextBox1.AppendText(vbTab)
+        TextBox1.AppendText(TextBox3.Text)
+        If TextBox4.Enabled Then
+            TextBox1.AppendText(vbTab)
+            TextBox1.AppendText(TextBox4.Text)
+        End If
+        If TextBox5.Enabled Then
+            TextBox1.AppendText(vbTab)
+            TextBox1.AppendText(TextBox5.Text)
+        End If
+
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        TextBox5.Text = "70"
+    End Sub
 End Class

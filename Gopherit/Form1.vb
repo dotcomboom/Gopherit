0b664a75c0b3446fff0e5b5a87d7f994e96bb74c
Eric T <Eric@SPIN-WIN>
2018-06-19 20:11:55 -0500

    when activating bookmark clear listview selected item

--- Gopherit/Form1.vb
+++ Gopherit/Form1.vb
@@ -247,11 +247,12 @@ Public Class Form1
         End If
     End Sub
 
-    Private Sub ListView1_DoubleClick(sender As Object, e As EventArgs) Handles ListView1.DoubleClick, ListView1.ItemActivate
+    Private Sub ListView1_DoubleClick(sender As Object, e As EventArgs) Handles ListView1.ItemActivate
         If ListView1.SelectedItems.Count = 1 Then
             ComboBox1.Text = ListView1.SelectedItems.Item(0).Text
             Go(sender, e)
         End If
+        ListView1.SelectedItems.Clear()
     End Sub
 
     Private Sub RemoveBookmarkToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RemoveBookmarkToolStripMenuItem.Click
